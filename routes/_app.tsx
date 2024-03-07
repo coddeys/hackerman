63f807be87cfe6ba69acc411bd42997d218ca22c
RyanSimons <ryan.simons@nedap.com>
2024-03-07 11:44:41 +0100

    added starting page for sign in

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -59,7 +59,7 @@ function Header() {
             <div className="hidden sm:flex">
               <a
                 className="rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600"
-                href="#"
+                href="/signin"
               >
                 Register
               </a>
