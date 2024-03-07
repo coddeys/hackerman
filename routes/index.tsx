4f8d16c66eff96a8e95e8e3f1543032cb466c530
Dima S <dima@sukhikh.xyz>
2024-03-07 11:08:46 +0100

    Header :-)

--- routes/index.tsx
+++ routes/index.tsx
@@ -4,7 +4,8 @@ import Counter from "../islands/Counter.tsx";
 export default function Home() {
   const count = useSignal(3);
   return (
-    <div class="px-4 py-8 mx-auto bg-[#86efac]">
+    
+    <div class="px-164 py-8 mx-auto bg-[#86efac]">
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
         <img
           class="my-6"
@@ -13,7 +14,7 @@ export default function Home() {
           height="128"
           alt="the Fresh logo: a sliced lemon dripping with juice"
         />
-        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
+        <h1 class="text-sm font-bold">Welcome to Fresh</h1>
         <p class="my-4">
           Try updating this message in the
           <code class="mx-2">./routes/index.tsx</code> file, and refresh.
@@ -23,3 +24,4 @@ export default function Home() {
     </div>
   );
 }
+
