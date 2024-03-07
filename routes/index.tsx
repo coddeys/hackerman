7aedce084208a0d5f34ee3aa7e42ce59c93cb77f
Dima S <dima@sukhikh.xyz>
2024-03-07 14:19:19 +0100

    deno fmt

--- routes/index.tsx
+++ routes/index.tsx
@@ -4,7 +4,6 @@ import Counter from "../islands/Counter.tsx";
 export default function Home() {
   const count = useSignal(3);
   return (
-    
     <div class="">
       <Banner />
     </div>
@@ -13,29 +12,29 @@ export default function Home() {
 
 function Banner() {
   return (
-      <section className="bg-gray-900 text-white">
-        <div className="mx-auto max-w-screen-xl px-2 py-32 lg:flex lg:h-screen lg:items-center">
-          <div className="mx-auto max-w-3xl text-center">
-            <h1
-                className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl"
+    <section className="bg-gray-900 text-white">
+      <div className="mx-auto max-w-screen-xl px-2 py-32 lg:flex lg:h-screen lg:items-center">
+        <div className="mx-auto max-w-3xl text-center">
+          <h1 className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl">
+            SUPER COOL HACKATHONS
+          </h1>
+
+          <p className="mx-auto mt-4 max-w-xl sm:text-xl/relaxed">
+            Code like the wind, debug like a ninja, and hack like there's no
+            tomorrow! Welcome to Hackathons where sleep is optional and caffeine
+            is mandatory. Let's turn bugs into features and pizza into code!
+          </p>
+
+          <div className="mt-8 flex flex-wrap justify-center gap-4">
+            <a
+              className="block w-full rounded border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-white focus:outline-none focus:ring active:text-opacity-75 sm:w-auto"
+              href="/hackathons/new"
             >
-              SUPER COOL HACKATHONS
-            </h1>
-
-            <p className="mx-auto mt-4 max-w-xl sm:text-xl/relaxed">
-              Code like the wind, debug like a ninja, and hack like there's no tomorrow! Welcome to Hackathons where sleep is optional and caffeine is mandatory. Let's turn bugs into features and pizza into code!
-            </p>
-
-            <div className="mt-8 flex flex-wrap justify-center gap-4">
-              <a
-                  className="block w-full rounded border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-white focus:outline-none focus:ring active:text-opacity-75 sm:w-auto"
-                  href="/hackathons/new"
-              >
-                Create Hackathon
-              </a>
-            </div>
+              Create Hackathon
+            </a>
           </div>
         </div>
-      </section>
-  )
+      </div>
+    </section>
+  );
 }
